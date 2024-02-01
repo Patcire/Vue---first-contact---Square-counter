351d0ffed66bfedcf9c06e7770ce934405fd5b23
Patricio <pcifrey501@g.educaand.es>
2024-02-01 09:32:04 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Counter from './components/Counter.vue'
+export default {
+  components:{Counter} // en vue es necesario exportar los componentes hacia main
+}
+</script>
+
+<template>
+  <Counter></Counter>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,78 @@
+
+<script>
+  export default {
+   // props:['title'], una forma de declarar props
+  // otra forma es con un objeto
+
+    props:{ // este permite establecer el tipo, lo cual ofrece cierta validación
+      // ya que por consola le dirá al desarrollador cierto feedback del fallo
+      // OJO: Solo da feedback al dev, la app no se rompe ni le da feedback al usuario!
+
+      title: String,
+      initialNumber: {
+        type: Number,
+        // required: true,
+        default: 20
+      }
+    },
+
+
+    data(){
+      return{
+        counter:this.initialNumber, // paso un props a la variable reactiva
+        subtitle: "learning vue"
+      }
+    },
+
+    methods: {
+      getSquare(){
+        return this.counter * this.counter
+      },
+
+      increase(){
+        this.counter++
+      },
+
+      decrease(){
+        this.counter--
+      }
+
+    },
+
+    computed: {
+      getSquareComputed(){
+        return this.getSquare()
+      }
+    }
+
+  }
+</script>
+
+
+<template>
+
+  <section class="ejemplo">
+    <h1>Counter</h1>
+    <h6>{{subtitle}}</h6>
+    <p>{{counter}}<sup>2</sup> =  {{getSquareComputed}}</p>
+    <button v-on:click="increase">Incrementar</button>
+    <button v-on:click="decrease">disminuir</button>
+  </section>
+
+</template>
+
+<style scoped>
+
+.ejemplo{
+  font-size: xxx-large;
+
+}
+
+button{
+  height: 20%;
+  width: 60%;
+  background-color: aquamarine;
+  cursor: pointer;
+}
+
+</style>
